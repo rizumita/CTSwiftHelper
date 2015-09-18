3a5a9317db8eec517f17a6037bed414e38a1a926
Ryoichi Izumita <r.izumita@caph.jp>
2015-09-18 22:21:57 +0900

    Initial Commit

---
+++ CTSwiftHelper/CTSwiftHelper.h
@@ -0,0 +1,19 @@
+//
+//  CTSwiftHelper.h
+//  CTSwiftHelper
+//
+//  Created by 和泉田領一 on 2015/09/18.
+//  Copyright © 2015年 CAPH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CTSwiftHelper.
+FOUNDATION_EXPORT double CTSwiftHelperVersionNumber;
+
+//! Project version string for CTSwiftHelper.
+FOUNDATION_EXPORT const unsigned char CTSwiftHelperVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CTSwiftHelper/PublicHeader.h>
+
+
